752ec6470a4b55a33b7529788ea3741204c806a9
Nathalie <106367468+nathusvel09@users.noreply.github.com>
2024-01-24 15:14:59 +0000

    commit dogs nextjs

---
+++ dogs/src/app/components/dogs.js
@@ -0,0 +1,22 @@
+"use client"
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import style from "./dogs.module.css";
+
+function Dog() {
+    const [dog, setDog] = useState("/vercel.svg")
+
+    const url = "https://dog.ceo/api/breed/sheepdog/english/images/random"
+    useEffect(() => {
+        fetch(url).then(res => res.json()).then(data => {setDog(data.message)})
+    }, [])
+
+    return (
+        <div className={style.box}>
+            <h1>English Shepdog</h1>
+            <Image src={dog} alt="dog" width={500} height={500}  />
+        </div>
+    );
+}
+export default Dog;
